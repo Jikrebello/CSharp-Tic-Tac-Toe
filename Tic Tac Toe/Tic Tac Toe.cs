952004459b2229dd936fedc2e1410765d4c9d07a
James Rebello <Jikrebello.za@gmail.com>
2022-08-24 15:22:05 +0200

    Update Tic Tac Toe.cs

--- Tic Tac Toe/Tic Tac Toe.cs
+++ Tic Tac Toe/Tic Tac Toe.cs
@@ -11,7 +11,6 @@ namespace Tic_Tac_Toe
         // Used to generate a number between 0 and 1 for deciding which player goes first.
         readonly Random rand = new Random();
 
-        // ---CONSTRUCTOR---
         public Tic_Tac_Toe()
         {
             while (true)
@@ -25,7 +24,7 @@ namespace Tic_Tac_Toe
                 if (gameOver)
                 {
                     Console.WriteLine("Do you want to play again? Enter in Y for Yes or N for No.");
-                    string input = Console.ReadLine();
+                    var input = Console.ReadLine();
 
                     if (input.ToUpper() == "Y")
                     {
@@ -36,20 +35,18 @@ namespace Tic_Tac_Toe
                     else if (input.ToUpper() == "N")
                         break;
                     else
-                        Console.WriteLine("Please enter in a valid input.");
+                        Console.WriteLine("Please enter valid input.");
                 }
             }
         }
 
-        // --- METHODS ---
 
         /// <summary>
         /// Randomly determines which player will go first.
         /// </summary>
         void CalculateTurnOrder()
         {
-            int turnDecider;
-            turnDecider = rand.Next(0, 2);
+            var turnDecider = rand.Next(0, 2);
             if (turnDecider == 1)
                 player1Turn = true;
             else
@@ -64,41 +61,27 @@ namespace Tic_Tac_Toe
 
         void ResetBoard()
         {
-            int number = 1;
+            int coordinate = 1;
             for (int row = 0; row < 3; row++)
             {
                 for (int column = 0; column < 3; column++)
                 {
-                    board[row, column] = Convert.ToString(number);
-                    number++;
+                    board[row, column] = Convert.ToString(coordinate);
+                    coordinate++;
                 }
             }
         }
 
-        void DisplayBoard()
-        {
-            Console.WriteLine("     " + "|" + "     " + "|" + "     ");
-            Console.WriteLine("  " + board[0, 0] + "  |  " + board[0, 1] + "  |  " + board[0, 2]);
-            Console.WriteLine("_____" + "|" + "_____" + "|" + "_____");
-            Console.WriteLine("     " + "|" + "     " + "|" + "     ");
-            Console.WriteLine("  " + board[1, 0] + "  |  " + board[1, 1] + "  |  " + board[1, 2]);
-            Console.WriteLine("_____" + "|" + "_____" + "|" + "_____");
-            Console.WriteLine("     " + "|" + "     " + "|" + "     ");
-            Console.WriteLine("  " + board[2, 0] + "  |  " + board[2, 1] + "  |  " + board[2, 2]);
-            Console.WriteLine("     " + "|" + "     " + "|" + "     ");
-            Console.WriteLine();
-        }
-
         void GetInput()
         {
+            string input;
             while (!gameOver)
             {
                 if (player1Turn && !IsBoardFull())
                 {
-                    Console.Clear();
                     DisplayBoard();
-                    Console.WriteLine("Place an X for Player 1.");
-                    string input = Console.ReadLine();
+                    Console.WriteLine("Place an X for Player 1 at a numbered position on the board.");
+                    input = Console.ReadLine();
                     if (ReturnCoordinates(input).Item1 == -1) // ---INVALID COORDINATES FOR PLAYER 1---
                         continue;
                     else
@@ -110,10 +93,9 @@ namespace Tic_Tac_Toe
                 }
                 else if (!player1Turn && !IsBoardFull())
                 {
-                    Console.Clear();
                     DisplayBoard();
-                    Console.WriteLine("Place a O for Player 2.");
-                    string input = Console.ReadLine();
+                    Console.WriteLine("Place a O for Player 2 at a numbered position on the board.");
+                    input = Console.ReadLine();
                     if (ReturnCoordinates(input).Item1 == -1) // ---INVALID COORDINATES FOR PLAYER 2---
                         continue;
                     else
@@ -126,6 +108,7 @@ namespace Tic_Tac_Toe
                 // ---BOTH PLAYERS LOSE---
                 else
                 {
+                    DisplayBoard();
                     Console.WriteLine("No one wins :(");
                     gameOver = true;
                 }
@@ -153,11 +136,26 @@ namespace Tic_Tac_Toe
             return answer;
         }
 
+        void DisplayBoard()
+        {
+            Console.Clear();
+            Console.WriteLine("     " + "|" + "     " + "|" + "     ");
+            Console.WriteLine("  " + board[0, 0] + "  |  " + board[0, 1] + "  |  " + board[0, 2]);
+            Console.WriteLine("_____" + "|" + "_____" + "|" + "_____");
+            Console.WriteLine("     " + "|" + "     " + "|" + "     ");
+            Console.WriteLine("  " + board[1, 0] + "  |  " + board[1, 1] + "  |  " + board[1, 2]);
+            Console.WriteLine("_____" + "|" + "_____" + "|" + "_____");
+            Console.WriteLine("     " + "|" + "     " + "|" + "     ");
+            Console.WriteLine("  " + board[2, 0] + "  |  " + board[2, 1] + "  |  " + board[2, 2]);
+            Console.WriteLine("     " + "|" + "     " + "|" + "     ");
+            Console.WriteLine();
+        }
+
         /// <summary>
         /// Checks if the chosen numbered block is available in the board.
         /// </summary>
         /// <param name="_input">The value passed in by the player to occupy the selected block.</param>
-        /// <returns>The X and Y coordinates for the board if the block is available. If not, returns -1</returns>
+        /// <returns>The X and Y coordinates if the block on the board is available. If not, returns -1</returns>
         Tuple<int, int> ReturnCoordinates(string _input)
         {
             for (int row = 0; row < 3; row++)
@@ -167,45 +165,64 @@ namespace Tic_Tac_Toe
                     if (board[row, column] == _input)
                     {
                         // Create a tuple that holds the coordinates of the array where input was found and returns.
-                        Tuple<int, int> foundCoordinates = new Tuple<int, int>(row, column);
+                        var foundCoordinates = new Tuple<int, int>(row, column);
+
                         return foundCoordinates;
                     }
                 }
             }
             // If the players input was not found in the array, return a tuple with coordinates outside the bounds of the array for checks.
-            Tuple<int, int> notFoundCoordinates = new Tuple<int, int>(-1, -1);
+            var notFoundCoordinates = new Tuple<int, int>(-1, -1);
+
             return notFoundCoordinates;
         }
 
         void CheckAndPrintWinner()
         {
-            // --- WIN CONDITIONS FOR CROSSES PLAYER ---
-            if (CheckWinCondition("X"))
+            if (player1Turn)
             {
-                Console.WriteLine("Crosses wins!");
-                gameOver = true;
+                if (CheckWinCondition())
+                {
+                    DisplayBoard();
+                    Console.WriteLine("Player 1 wins!");
+                    gameOver = true;
+                }
             }
-            // --- WIN CONDITIONS FOR NOUGHTS PLAYER ---
-            else if (CheckWinCondition("O"))
+            else
             {
-                Console.WriteLine("Naught's wins!");
-                gameOver = true;
+                if (CheckWinCondition())
+                {
+                    DisplayBoard();
+                    Console.WriteLine("Player 2 wins!");
+                    gameOver = true;
+                }
             }
+
         }
 
-        bool CheckWinCondition(string player)
+        bool CheckWinCondition()
         {
-            if (board[0, 0] == player && board[0, 1] == player && board[0, 2] == player ||    // TOP ROW.
-                board[1, 0] == player && board[1, 1] == player && board[1, 2] == player ||    // MIDDLE ROW.
-                board[2, 0] == player && board[2, 1] == player && board[2, 2] == player ||    // BOTTOM ROW.
-                board[0, 0] == player && board[1, 0] == player && board[2, 0] == player ||    // LEFT COLUMN.
-                board[0, 1] == player && board[1, 1] == player && board[2, 1] == player ||    // MIDDLE COLUMN.
-                board[0, 0] == player && board[1, 2] == player && board[2, 2] == player ||    // RIGHT COLUMN.
-                board[0, 2] == player && board[1, 1] == player && board[2, 0] == player ||    // DIAGONAL TOP RIGHT BOTTOM LEFT.
-                board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)      // DIAGONAL TOP LEFT BOTTOM RIGHT.
+            for (int i = 0; i < 3; i++)
+            {
+                // --- HORIZONTAL CHECK ---
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    return true;
+                }
+                // --- VERTICAL CHECK ---
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    return true;
+                }
+            }
+
+            // --- DIAGONAL CHECKS ---
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
                 return true;
-            else
-                return false;
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+                return true;
+            return false;
         }
     }
 }
+
